acdfaa2ddf529eda552fc906f975bf7e2d8d3560
Omri <omri.hermon@gmail.com>
2025-01-07 21:25:23 +0200

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950 text-white">
         <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[3rem]">
           mooseguessr
         </h1>
