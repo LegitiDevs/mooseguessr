ee141d66c83e166d24129131bfbe71004d251278
Omri <omri.hermon@gmail.com>
2025-01-07 21:31:20 +0200

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "~/components/ui/button";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950 text-white">
-        <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[3rem]">
+        <h1 className="text-10xl font-extrabold tracking-tight text-white sm:text-[4rem]">
           mooseguessr
         </h1>
+        <Button>Start</Button>
     </main>
   );
 }
