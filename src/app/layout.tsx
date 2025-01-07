35e620e940bd5ef4286c2f94fbea96ea03c9e6f6
Omri <omri.hermon@gmail.com>
2025-01-07 20:23:32 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "mooseguessr",
+  description: "A GeoGuessr like-game for the Minecraft server legitimoose.com",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          mooseguessr
+        </h1>
+    </main>
+  );
+}
